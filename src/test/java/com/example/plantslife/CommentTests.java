c86caea1540dfa99d2612e1768df99e1082606be
Mohammed Mutaliev <mutaliev@kth.se>
2021-01-13 17:05:54 +0100

    runwith removed

--- src/test/java/com/example/plantslife/CommentTests.java
+++ src/test/java/com/example/plantslife/CommentTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
